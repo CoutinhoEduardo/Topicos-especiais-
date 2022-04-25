fe2131cd17a4a1d1c90d9ad8192c4416555fe442
CoutinhoEduardo <eduardo.viniciusc26@gmail.com>
2022-04-25 00:12:59 -0300

    Criadas paginas iniciais e logins

---
+++ script.js
@@ -0,0 +1,18 @@
+function logar(){
+    alert("cARAALHO")
+}
+
+function portalAluno(){
+    alert("Portal do Aluno!")
+    location.href="/login/loginAluno.html"
+}
+
+function portalProfessor(){
+    alert("Portal do Professor!")
+    location.href="/login/loginProfessor.html"
+}
+
+function portalPais(){
+    alert("Portal do Pais!")
+    location.href="/login/loginPais.html"
+}
