72692d3e90a5cfe69027022f5483d871753754c5
CoutinhoEduardo <eduardo.viniciusc26@gmail.com>
2022-04-28 02:04:17 -0300

    front terminado

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-function logar(){
-    alert("cuccu")
-    location.href="principal/principalAlunos.html"
-}
 
-function portalAluno(){
-    alert("Portal do Aluno!")
+    var cpf = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+function loginAluno(){
+    alert("Login do Aluno!")
     location.href="/login/loginAluno.html"
 }
 
-function portalProfessor(){
-    alert("Portal do Professor!")
-    location.href="/login/loginProfessor.html"
+function loginDocentes(){
+    alert("Login Docentes!")
+    location.href="/login/loginDocentes.html"
 }
 
-function portalPais(){
-    alert("Portal do Pais!")
+function loginPais(){
+    alert("Login dos Pais!")
     location.href="/login/loginPais.html"
 }
+
+
+function portalAluno(){
+
+        if(cpf == "114" && senha == "123"){
+        alert("portal do aluno!!")
+        location.href="/principal/aluno/principalAluno"
+         }else{
+        alert("erro")
+         }
+        }
+
+function portalDocentes(){
+    if(cpf == "123" && senha == "123"){
+        alert("portal do aluno!!")
+        location.href="/principal/aluno/principalFinanceiro.html"
+    }
+    if(cpf == "456" && senha == "123"){
+        alert("portal do professor!!")
+        location.href="/principal/professor/principalProfessor.html"
+    }if(cpf == "789" && senha == "123"){
+        alert("portal do financeiro!!")
+        location.href="/principal/secretaria/principalSecretaria.html"
+    }
+        
+    }
